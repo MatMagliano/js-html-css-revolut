648b4dd64ca8dc5d55aa569abd43a9fd8f93ecf2
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-20 16:58:12 +0100

    addClass

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // alert('hello');
+
+$(document).ready(
+  function() {
+    $('.relative > a').mouseenter(
+      function() {
+        $(this).next('.accounts').addClass('flex');
+      }
+    )
+  }
+);
